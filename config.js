efe69b4346d7e80ad62e1bca8587993d5c35bb49
Vikas Nathani <nathanivikas890@example.com>
2026-04-09 09:31:20 +0000

    just changed the external ip

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // API Configuration
 const CONFIG = {
-    TRIAGE_API: 'http://34.180.37.249:9000',
-    SUMMARY_API: 'http://34.180.37.249:8004',
-    REDIS_API:   'http://34.180.37.249:8000',
+    TRIAGE_API: 'http://34.14.197.45:9000',
+    SUMMARY_API: 'http://34.14.197.45:8004',
+    REDIS_API:   'http://34.14.197.45:8000',
     DEFAULT_PATIENT_ID: '791427b4-9cc4-8bcc-3fee-e3e14b6d3fea'
 };
 
